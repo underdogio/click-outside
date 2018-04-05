d7847597b574173ef9333c7084af339d77efa784
Mario Pabon <me@mariopabon.com>
2018-04-05 14:25:38 -0400

    Use a single event listener for multiple listeners

--- index.js
+++ index.js
@@ -1,26 +1,66 @@
-// Invokes a callback when a click happens outside of the target element
-module.exports = function onClickOutside (element, fn) {
-  // Return if we are not in a browser
-  if (typeof document === 'undefined') {
-    return
-  }
+// Maintain a list of [element, listener] pairs. When a document click event is fired, we'll check all of the listeners
+// in this list and invoke listeners whose element was clicked outside of.
+var listeners = []
 
-  function listener (event) {
-    var target = event.target
+function onDocumentClick (event) {
+  var target = event.target
+
+  listeners.forEach(function (elementListener) {
+    var element = elementListener[0]
+    var listener = elementListener[1]
 
     // Return if click happened within or on the target element
     if (element.contains(target) || element === target) {
       return
     }
 
-    // Invoke callback
-    fn(event)
+    // Invoke listener with original event
+    listener(event)
+  })
+}
+
+function addListener (element, listener) {
+  // Add document click event listener if we don't have any listeners yet
+  if (listeners.length === 0) {
+    document.addEventListener('click', onDocumentClick)
   }
 
-  document.addEventListener('click', listener)
+  listeners.push([element, listener])
+}
+
+function removeListener (element, listener) {
+  var listenerIndex = -1
+  listeners.some(function (elementListener, index) {
+    if (elementListener[0] === element && elementListener[1] === listener) {
+      listenerIndex = index
+      return true
+    }
+  })
+
+  if (listenerIndex < 0) {
+    return false
+  }
+
+  listeners.splice(listenerIndex, 1)
+
+  if (listeners.length === 0) {
+    document.removeEventListener('click', onDocumentClick)
+  }
+
+  return true
+}
+
+// Invokes a callback when a click happens outside of the target element
+module.exports = function clickOutside (element, listener) {
+  // Return if we are not in a browser
+  if (typeof document === 'undefined') {
+    return
+  }
+
+  addListener(element, listener)
 
   // Return a function to remove the event listener
   return function removeEventListener () {
-    document.removeEventListener('click', listener)
+    return removeListener(element, listener)
   }
 }
